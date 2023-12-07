1a9d414fd0bcc5dcb114df38847a74e01802a229
Nam Nguyen <namgaxilem@gmail.com>
2023-12-07 22:31:25 +0700

    add about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -5,7 +5,17 @@ export default function About() {
   const location = useLocation();
 
   const isDetailPage = () => {
+    if (location.pathname === "/about") {
+      return true;
+    }
     return false;
   };
-  return <main className={`${styles.detailContainer} ${isDetailPage() ? styles.show : ""}`}>About</main>;
+
+  return (
+    <main className={`${styles.detailContainer} ${isDetailPage() ? styles.show : ""}`}>
+      {[...Array(100)].map((e) => (
+        <div key={e}>e</div>
+      ))}
+    </main>
+  );
 }
