303375b39b1b0ee630ae2ddcd84901c1abc07694
Nam Nguyen <namgaxilem@gmail.com>
2023-12-23 16:26:43 +0700

    remove unused images | transition for header black

--- src/pages/Home/Header/index.tsx
+++ src/pages/Home/Header/index.tsx
@@ -87,8 +87,15 @@ export default function Header() {
       </header>
 
       <span className={`${styles.backArrow} ${isDetailPage() ? "" : styles.hide}`} onClick={goBack}>
-        <svg className={` ${isDetailPage() ? "" : styles.hide}`} xmlns="http://www.w3.org/2000/svg" width="100%" height="34" viewBox="0 0 67 34">
-          <g fill="none" fill-rule="evenodd" stroke={`${headerColorChange ? "black" : "white"}`} stroke-linecap="round" transform="translate(2 1)">
+        <svg className={` ${isDetailPage() ? "" : styles.hide}`} width="100%" height="34" viewBox="0 0 67 34">
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            className={styles.gEle}
+            stroke={`${headerColorChange ? "black" : "white"}`}
+            stroke-linecap="round"
+            transform="translate(2 1)"
+          >
             <path stroke-width="2" d="M0,15.5533333 L64,15.5533333"></path>
             <polyline stroke-width="2" points="15.556 0 0 15.556 15.556 31.111"></polyline>
           </g>
