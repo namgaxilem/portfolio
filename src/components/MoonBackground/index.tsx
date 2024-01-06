138e991efc29314e212bfb840fe9961006a189c5
Nam Nguyen <namgaxilem@gmail.com>
2024-01-06 20:39:59 +0700

    remove scroll by time function

--- src/components/MoonBackground/index.tsx
+++ src/components/MoonBackground/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 
 const MOON_ID = "moonContainer";
@@ -51,6 +51,7 @@ export default function MoonBackground() {
   const [isSMScreen, setIsSMScreen] = useState(false);
   const [stars] = useState(dotStars());
   const [initMoonPos, setInitMoonPos] = useState<DOMRect>();
+  const resetMoving = useRef(Math.random());
 
   useEffect(() => {
     function handleScreenResize() {
@@ -65,61 +66,29 @@ export default function MoonBackground() {
     }
 
     async function handleMouseMove(e) {
+      resetMoving.current = Math.random();
       if (!initMoonPos) {
         return;
       }
 
-      async function scrollByTime(endX, endY, time) {
-        const moonContainer = window.document.getElementById(MOON_ID);
-        const startTime = performance.now();
-        const endTime = startTime + time;
-        const startX = Number(window.getComputedStyle(moonContainer).getPropertyValue("left").replace("px", ""));
-        const startY = Number(window.getComputedStyle(moonContainer).getPropertyValue("top").replace("px", ""));
-        const distanceX = startX - endX;
-        const distanceY = startY - endY;
-        let currentX = startX;
-        let currentY = startY;
-
-        while (performance.now() < endTime) {
-          const progress = (performance.now() - startTime) / time;
-          currentX = startX - progress * distanceX;
-          currentY = startY - progress * distanceY;
-          moonContainer.style.transform = `translate3d(${currentX}px, ${currentY}, 0px)`;
-          console.log("currentX", currentX);
-
-          requestAnimationFrame(() =>{});
-          // moonContainer.style.left = `${currentX}px`;
-          // moonContainer.style.top = `${currentY}px`;
-          await new Promise(requestAnimationFrame);
-        }
-        moonContainer.style.transform = `translate3d(${endX}px, ${endY}px, 0px)`;
-        // moonContainer.style.left = `${endX}px`;
-        // moonContainer.style.top = `${endY}px`;
-      }
-
       if (e.pointerType === "mouse") {
-        const moonContainer = document.getElementById(MOON_ID);
         const centerOfMoonX = (initMoonPos.right - initMoonPos.left) / 2;
         const centerOfMoonY = (initMoonPos.bottom - initMoonPos.top) / 2;
         const distanceX = centerOfMoonX - e.x;
         const distanceY = centerOfMoonY - e.y;
         const newPosition = { left: `${initMoonPos.left}px`, top: `${initMoonPos.top}px` };
         if (distanceX !== 0) {
-          newPosition.left = `${((centerOfMoonX - e.x) / centerOfMoonX) * 60}px`;
+          newPosition.left = `${((centerOfMoonX - e.x) / centerOfMoonX) * 20}px`;
         } else {
           newPosition.left = `${initMoonPos.left}px`;
         }
         if (distanceY !== 0) {
-          newPosition.top = `${((centerOfMoonY - e.y) / centerOfMoonY) * 60}px`;
+          newPosition.top = `${((centerOfMoonY - e.y) / centerOfMoonY) * 20}px`;
         } else {
           newPosition.top = `${initMoonPos.top}px`;
         }
-        // moonContainer.style.transform = `translate3d(${newPosition.left}, ${newPosition.top}, 0px)`;
-        await scrollByTime(
-          newPosition.left.replace("px", ""),
-          newPosition.top.replace("px", ""),
-          4000
-        );
+        const moonContainer = window.document.getElementById(MOON_ID);
+        moonContainer.style.transform = `translate3d(${newPosition.left}, ${newPosition.top}, 0px)`;
       } else if (e.pointerType === "touch") {
         e.preventDefault();
       }
@@ -138,7 +107,6 @@ export default function MoonBackground() {
   useLayoutEffect(() => {
     function handleScreenResize() {
       const moonCircleBoundingClient = (document.getElementById(MOON_ID).firstElementChild.firstChild as SVGCircleElement).getBoundingClientRect();
-      console.log("condime", moonCircleBoundingClient);
       setInitMoonPos(moonCircleBoundingClient);
     }
     handleScreenResize();
